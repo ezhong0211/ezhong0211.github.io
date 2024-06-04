bc807141bf44969f2d62368e3b941929010d37d4
Eric Zhong <ejz25@cornell.edu>
2024-06-04 02:14:04 -0700

    deleted unecessary code

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-// function showScreen(screenId) {
-//   document.querySelectorAll(".screen").forEach((screen) => {
-//     screen.classList.remove("active");
-//   });
-//   document.getElementById(screenId).classList.add("active");
-// }
-
 function openResume() {
   open("./Eric_Zhong_Resume.pdf");
 }
