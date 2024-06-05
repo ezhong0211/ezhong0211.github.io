e35768d028ad14ad0058550ffe734978cd317ca5
Eric Zhong <ejz25@cornell.edu>
2024-06-04 21:16:58 -0700

    fixed warning

--- script.js
+++ script.js
@@ -3,5 +3,7 @@ function openResume() {
 }
 
 function isMobile(){
-  alert(navigator.userAgent);
+  var mobile = (/iphone|ipad|ipod|android|blackberry|mini|windows\sce|palm/i.test(navigator.userAgent.toLowerCase()));
+  if (mobile) 
+      alert("Visit this on a Computer for Better View");             
 }
