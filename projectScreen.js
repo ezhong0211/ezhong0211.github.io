4544c069ff76a48184f37b9fe2bf7043fa841c80
Eric Zhong <ejz25@cornell.edu>
2024-06-04 21:38:32 -0700

    added analytics

--- projectScreen.js
+++ projectScreen.js
@@ -1,14 +1,14 @@
 function showOther(className) {
-  var frontContName = document.querySelector(`.${className}pic`);
-  console.log(frontContName);
-  var backContName = document.querySelector(`.${className}Desc`);
-  if (frontContName.style.display == "none") {
-    frontContName.style.display = "block";
-    backContName.style.display = "none";
-  } else {
-    frontContName.style.display = "none";
-    backContName.style.display = "block";
-  }
+  // var frontContName = document.querySelector(`.${className}pic`);
+  // console.log(frontContName);
+  // var backContName = document.querySelector(`.${className}Desc`);
+  // if (frontContName.style.display == "none") {
+  //   frontContName.style.display = "block";
+  //   backContName.style.display = "none";
+  // } else {
+  //   frontContName.style.display = "none";
+  //   backContName.style.display = "block";
+  // }
 }
 
 function openResume() {
