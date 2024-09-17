15f612e69ccc7822ff69eaa2ca2eb06b4da7e42b
Eric Zhong <ejz25@cornell.edu>
2024-09-17 19:32:07 -0400

    changed unused code

--- experienceScreen.js
+++ experienceScreen.js
@@ -1,16 +1,3 @@
-function showOther(className) {
-  // var frontContName = document.querySelector(`.${className}pic`);
-  // console.log(frontContName);
-  // var backContName = document.querySelector(`.${className}Desc`);
-  // if (frontContName.style.display == "none") {
-  //   frontContName.style.display = "block";
-  //   backContName.style.display = "none";
-  // } else {
-  //   frontContName.style.display = "none";
-  //   backContName.style.display = "block";
-  // }
-}
-
 function openResume() {
   open("./Eric_Zhong_Resume.pdf");
   console.log("work")
